c23328788d307e16ab1e4335cfc081d58950e613
Sushil Choudhari <sushilchoudhari9922@gmail.com>
2021-09-29 14:50:09 +0530

    feat:added object database adding object in database to interpret in text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./styles.css";
 
+const objectdb = {
+  "📋": "clipboard",
+  "📎": "paper clip",
+  "📅": "claender",
+  "🧭": "compass",
+  "🎈": "balloon"
+};
+
+var objectarray = Object.keys(objectdb);
+
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Object Decipherer</h1>
+      <input></input>
+    </div>
+  );
 }
