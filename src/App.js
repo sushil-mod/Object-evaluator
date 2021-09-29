2e0c2e4abf0b8c707b3e92a11aa35acbc0e0ba69
Sushil Choudhari <sushilchoudhari9922@gmail.com>
2021-09-29 14:57:20 +0530

    feat: added events and i/o

--- src/App.js
+++ src/App.js
@@ -11,10 +11,25 @@ const objectdb = {
 var objectarray = Object.keys(objectdb);
 
 export default function App() {
+  function onchangehandler(event) {
+    console.log(event.target.value);
+  }
+
+  function onclickhandler() {
+    console.log("clicked");
+  }
+
   return (
     <div className="App">
       <h1>Object Decipherer</h1>
-      <input></input>
+      <input onChange={onchangehandler}></input>
+      <div>
+        {objectarray.map((item) => (
+          <span key={item} onClick={onclickhandler}>
+            {item}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
